7853ad2e2efd07ddef97c41d5cb54738d6779d06
rubendehaas <rdehaas92@gmail.com>
2014-12-30 11:36:10 +0100

    Window CRUD - section

---
+++ MVVMAppie/MVVMAppie/ProductAddWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MVVMAppie
+{
+    /// <summary>
+    /// Interaction logic for ProductAddWindow.xaml
+    /// </summary>
+    public partial class ProductAddWindow : Window
+    {
+        public ProductAddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddSectionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string valueName = SectionNameTB.Text;
+        }
+    }
+}
