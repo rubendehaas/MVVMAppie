d3e7951ed115d8a671d6dc7e0ead51881a9a1a99
rubendehaas <rdehaas92@gmail.com>
2015-01-11 16:12:36 +0100

    Unit testing setup

---
+++ MVVMAppie/UnitTest/SectionViewModelTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMAppie.ViewModel;
+using MVVMAppie.Model;
+using System.Threading;
+using Moq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class SectionViewModelTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mockRepository = new MockRepository(MockBehavior.Loose);
+            var mockMessenger = mockRepository.Create<Database>();
+            var vm = new SectionsVM(mockMessenger.Object);
+
+            Assert.IsTrue(vm.Sections.Count >= 0);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+        }
+    }
+}
